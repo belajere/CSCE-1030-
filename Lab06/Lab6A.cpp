a3a0d1f53f77f09b4fea9276e7a51ee72b6e6e9c
belajere <44950322+belajere@users.noreply.github.com>
2023-09-11 19:41:15 -0500

    Add files via upload

---
+++ Lab06/Lab6A.cpp
@@ -0,0 +1,24 @@
+/* Author: Bela Jere (belajere@my.unt.edu)
+   Date: 06/13/2023
+   Instructor: Dr.Shrestha
+   Description: creating random numbers and converting to characters
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main () {
+
+int arr[5];
+int i;
+srand(time(0));
+for(i=0;i<5;i++) {
+    arr[i] = rand()%26+65;
+}
+for(i=0;i<5;i++) {
+cout<<static_cast<char>(arr[i]);
+}
+cout<<endl;
+return 0;
+}
